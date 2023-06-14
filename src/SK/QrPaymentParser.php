4e95e89bf121c3a64cf8ecf4b85767238a8fa232
MimoGraphix <mimographix@gmail.com>
2023-06-14 07:46:17 +0200

    due date exception

--- src/SK/QrPaymentParser.php
+++ src/SK/QrPaymentParser.php
@@ -2,12 +2,9 @@
 
 namespace Uctoplus\QrPaymentWrapper\SK;
 
-use Rikudou\Iban\Iban\IBAN;
-use Rikudou\Iban\Iban\IbanInterface;
+use DateTime;
 use rikudou\SkQrPayment\Iban\IbanBicPair;
 use rikudou\SkQrPayment\QrPayment;
-use rikudou\SkQrPayment\Xz\XzBinaryLocator;
-use rikudou\SkQrPayment\Xz\XzBinaryLocatorInterface;
 use Uctoplus\QrPaymentWrapper\Exceptions\QrParserException;
 use Uctoplus\QrPaymentWrapper\Interfaces\QrParserInterface;
 
@@ -32,41 +29,39 @@ class QrPaymentParser extends BaseQrParser implements QrParserInterface
 
         $explode = explode("\t", $step2);
 
-//        var_dump($explode);
-
         $payment = new QrPayment();
         $payment->setInternalId($explode[0]);
 
         $payment->setAmount($explode[3]);
         $payment->setCurrency($explode[4]);
-        $payment->setDueDate(\DateTime::createFromFormat("Ymd", $explode[5]));
+        if (!empty($explode[5])) {
+            $payment->setDueDate(DateTime::createFromFormat("Ymd", $explode[5]));
+        }
         $payment->setVariableSymbol($explode[6]);
         $payment->setConstantSymbol($explode[7]);
         $payment->setSpecificSymbol($explode[8]);
-        if(!empty($explode[9])) {
+        if (!empty($explode[9])) {
             var_dump($explode[9]);
         }
         $payment->setComment($explode[10]);
         $ibans_count = $explode[11];
 
         $pointer = 12;
-        for ($i=0; $i<$ibans_count; $i++){
-            $iban = new IbanBicPair($explode[$pointer], $explode[$pointer+1]);
+        for ($i = 0; $i < $ibans_count; $i++) {
+            $iban = new IbanBicPair($explode[$pointer], $explode[$pointer + 1]);
             $payment->addIban($iban);
             $pointer += 2;
         }
 
-        if(isset($explode[$pointer+1]))
-            $payment->setPayeeName($explode[$pointer+1]);
+        if (isset($explode[$pointer + 1]))
+            $payment->setPayeeName($explode[$pointer + 1]);
 
-        if(isset($explode[$pointer+2]))
-            $payment->setPayeeAddressLine1($explode[$pointer+2]);
+        if (isset($explode[$pointer + 2]))
+            $payment->setPayeeAddressLine1($explode[$pointer + 2]);
 
-        if(isset($explode[$pointer+3]))
-            $payment->setPayeeAddressLine2($explode[$pointer+3]);
+        if (isset($explode[$pointer + 3]))
+            $payment->setPayeeAddressLine2($explode[$pointer + 3]);
 
-
-//        var_dump($explode, $payment);
         return $payment;
     }
 }
